938872ff8d8f667a1f2faa7af4631683649047fd
spy <ajeshmb24@gmail.com>
2026-03-06 20:16:43 +0530

    Addinga data seeding module for all the userea in the data base for demo purposes

--- seed.py
+++ seed.py
@@ -1,6 +1,10 @@
 import requests
 
-FASTAPI_URL = "http://127.0.0.1:9111"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ENV_PATH = os.path.join(BASE_DIR, "..", ".env")
+load_dotenv(dotenv_path=ENV_PATH)
+
+FASTAPI_URL = os.getenv("FASTAPI_URL")
 
 files = {
     "file": open("Sample_data.csv", "rb")
