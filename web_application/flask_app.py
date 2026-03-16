276d3a9e443023da6d384113723d5924967f93df
spy <ajeshmb24@gmail.com>
2026-03-16 21:12:45 +0530

    Added frond end nice and green

--- web_application/flask_app.py
+++ web_application/flask_app.py
@@ -11,7 +11,7 @@ ENV_PATH = os.path.join(BASE_DIR, "..", ".env")
 
 load_dotenv(dotenv_path=ENV_PATH)
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 app.secret_key = os.getenv("FLASK_SECRET_KEY")
 FASTAPI_URL = os.getenv("FASTAPI_URL")
